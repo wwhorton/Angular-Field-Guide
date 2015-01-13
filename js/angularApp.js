cd77e80d65d58b714f38724901c0f24e917556f6
Bill Whorton <wwhorton@chesapeakebay.net>
2015-01-13 16:14:21 -0500

    Added stub angular.app to Category page.

---
+++ js/angularApp.js
@@ -0,0 +1,9 @@
+(function(){
+	var app = angular.module('category', [ ]);
+	
+	app.controller('CategoryFilter', function(){
+		this.sampleText = "Sample Text means this sucker's workin'!!";
+		
+	});
+
+})();
