edd0f49a31501c458ff233db89a05754ae702c02
Bill Whorton <whorton@gmail.com>
2015-01-16 13:15:49 -0500

    Implements beginning of category filter by subtype.

--- js/angularApp.js
+++ js/angularApp.js
@@ -1,17 +1,7 @@
 (function(){
 	var category = angular.module('category', []);
 	
-	var options = [ "Raptors", "Other", "Sea & Shorebirds", "Wading", "Waterfowl" ];
-	
-	/*var entries = [
-	{"image": "http://placehold.it/300x200", "name": "Albatross", "scientificName": "Diomedea exulans", "subtype": "other"},
-	{"image": "http://placehold.it/300x200", "name": "Monkey", "scientificName": "Diomedea exulans", "subtype": "raptor"},
-	{"image": "http://placehold.it/300x200", "name": "Lorakeet", "scientificName": "Diomedea exulans", "subtype": "raptor"},
-	{"image": "http://placehold.it/300x200", "name": "Rockfish", "scientificName": "Diomedea exulans", "subtype": "waterfowl"},
-	{"image": "http://placehold.it/300x200", "name": "Capybara", "scientificName": "Diomedea exulans", "subtype": "sea & shorebirds"},
-	{"image": "http://placehold.it/300x200", "name": "Pigeon", "scientificName": "Diomedea exulans", "subtype": "other"},
-	{"image": "http://placehold.it/300x200", "name": "Sand Dollar", "scientificName": "Diomedea exulans", "subtype": "other"}
-	];*/
+	var options = [ {name: "Raptors", id: '22'}, {name: "Other", id: '19'}, "Sea & Shorebirds", "Wading", "Waterfowl" ];
 	
 	category.controller('CategoryFilter', ['$scope', '$http', function( $scope, $http ){
 		$scope.options = options;
