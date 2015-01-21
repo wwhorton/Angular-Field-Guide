2b7131e19507498b3534e782273cf7ceda3456e4
Bill Whorton <wwhorton@chesapeakebay.net>
2015-01-21 12:01:45 -0500

    Working on JSON output from channel tags for Category.

--- js/angularApp.js
+++ js/angularApp.js
@@ -1,11 +1,11 @@
 (function(){
 	var category = angular.module('category', []);
 	
-	var options = [ {name: "Raptors", id: '22'}, {name: "Other", id: '19'}, "Sea & Shorebirds", "Wading", "Waterfowl" ];
+	var options = [ {name: "Raptors", id: '22'}, {name: "Other", id: '19'}, {name: "Sea & Shorebirds", id: '20'}, {name: "Wading", id: '21'}, {name: "Waterfowl", id: '18'} ];
 	
 	category.controller('CategoryFilter', ['$scope', '$http', function( $scope, $http ){
 		$scope.options = options;
-    $http.get('http://localhost/API/entries').success( function( data ){
+    $http.get('http://localhost/API/type').success( function( data ){
 
       $scope.entries = data;
     });    
