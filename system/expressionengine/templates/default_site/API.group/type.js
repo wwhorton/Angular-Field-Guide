87ac937b55991154cf0ad1ba7ebd0ae4dbf386f2
Bill Whorton <wwhorton@chesapeakebay.net>
2015-02-02 12:02:03 -0500

    Adds subtype as json field to leverage filter behavior.

--- system/expressionengine/templates/default_site/API.group/type.js
+++ system/expressionengine/templates/default_site/API.group/type.js
@@ -1,10 +1,10 @@
 [
-{exp:channel:entries channel="field_guide_types" backspace="2" url_title="{segment_3}"}{if subtypes:count > 0}{subtypes}{subtypes:parents field="type"}
+{exp:channel:entries channel="field_guide_types" backspace="2" url_title="{segment_3}"}{subtypes}{if subtypes:count != '0'}{subtypes:parents field="type"}
   {
   "title": "{subtypes:parents:title}",
+  "type": "{subtypes:title}",
   "url": "{subtypes:parents:url_title}",
-  "image": "{subtypes:parents:media}{subtypes:parents:media:image}{/subtypes:parents:media}",
   "scientific_name": "{subtypes:parents:scientific_name}"
   },
-{/subtypes:parents}{/subtypes}{/if}{/exp:channel:entries}
+{/subtypes:parents}{/if}{/subtypes}{/exp:channel:entries}
 ]
