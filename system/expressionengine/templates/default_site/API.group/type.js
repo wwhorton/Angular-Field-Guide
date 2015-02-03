8e38e716c02348b98881751df2583072627b04b2
Bill Whorton <wwhorton@chesapeakebay.net>
2015-02-03 10:23:09 -0500

    Changes filter to pull all entries based on type and sort via subtype.

--- system/expressionengine/templates/default_site/API.group/type.js
+++ system/expressionengine/templates/default_site/API.group/type.js
@@ -1,10 +1,10 @@
 [
-{exp:channel:entries channel="field_guide_types" backspace="2" url_title="{segment_3}"}{subtypes}{if subtypes:count != '0'}{subtypes:parents field="type"}
+{exp:channel:entries channel="field_guide_types" backspace="2" url_title="{segment_3}"}{subtypes}{subtypes:parents field="type"}
   {
   "title": "{subtypes:parents:title}",
   "type": "{subtypes:title}",
   "url": "{subtypes:parents:url_title}",
   "scientific_name": "{subtypes:parents:scientific_name}"
   },
-{/subtypes:parents}{/if}{/subtypes}{/exp:channel:entries}
+{/subtypes:parents}{/subtypes}{/exp:channel:entries}
 ]
