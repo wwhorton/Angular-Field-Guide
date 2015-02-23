65586f410ddbcd3de8bd720c8206049bb2682c5c
Bill Whorton <wwhorton@chesapeakebay.net>
2015-02-23 15:52:24 -0500

    Further routing implementation and removal of EE routingin .htaccess file...

--- js/angularApp.js
+++ js/angularApp.js
@@ -1,25 +1,49 @@
 (function(){
-	var category = angular.module('category', [ 'ngRoute' ]);
-  //var options = [ "Fish", "Birds", "Algae", "Bay Grasses (SAV)", "Insects", "Invertebrates", "Mammals", "Plants & Trees", "Reptiles & Amphibians" ];
-  var options = [ "Raptors", "Wading", "Waterfowl", "Sea & Shorebirds", "Other" ];
-  category.factory( 'FilterOptions', [ '$q', '$http', function( $q, $http ){
-    var _options = [];
-    var getOptions = function(){
-        var deferred = $q.defer();
-        $http.get( 'http://localhost/API/subtype' ).success( function( data ){
-          deferred.resolve( data );
-        });
-        return deferred.promise;
-      };
-    return {
-      getOptions: getOptions
-    };
-  }]);
-  
-  category.config(['$routeProvider', 
-    function($routeProvider) {
+	var fieldGuide = angular.module('fieldGuide', [ 'ngRoute', 'mm.foundation' ]);
+
+  var types = [ 
+      { 'name' : 'Algae',
+        'subtypes' : [ 'All' ]
+      },
+      { 'name' : 'Bay Grasses (SAV)',
+        'subtypes' : [ 'All', 'Low Salinity', 'Medium Salinity', 'High Salinity' ]
+      },
+      { 'name' : 'Birds',
+        'subtypes' : [ 'All', 'Raptors', 'Other', 'Waterfowl', 'Wading', 'Sea & Shorebirds' ]
+      },
+      { 'name' : 'Fish',
+        'subtypes' : [ 'All', 'Freshwater Fish', 'Estuarine Fish', 'Saltwater Fish' ]
+      },
+      { 'name' : 'Insects',
+        'subtypes' : [ 'All' ]
+      },
+      { 'name' : 'Invertebrates', 
+        'subtypes' : [ 'All', 'Arthropods', 'Mollusks', 'Other Invertebrates' ]
+      },
+      { 'name' : 'Mammals',
+        'subtypes' : [ 'All', 'Aquatic', 'Semi-Aquatic', 'Land', 'Flying' ]
+      },
+      { 'name' : 'Plants & Trees',
+        'subtypes' : [ 'All', 'Flowers', 'Trees & Shrubs', 'Aquatic & Wetland Plants' ]
+      },
+      { 'name' : 'Reptiles & Amphibians',
+        'subtypes' : [ 'All', 'Amphibians', 'Reptiles' ]
+      }
+    ];
+  var habitats = [ 'Aquatic Reefs & Pilings', 'Beaches & Tidal Flats', 'Forests & Uplands', 'Marshes & Wetlands', 'Open Waters', 'Shallow Waters', 'Streams & Rivers' ];
+    
+  fieldGuide.config(['$routeProvider', '$locationProvider', 
+    function($routeProvider, $locationProvider) {
       $routeProvider.
+        when( '/', {
+          templateUrl: '/app/partials/start.html',
+          controller: 'StartCtrl'
+        }).
         when( '/category/:category', {
+          templateUrl: '/app/partials/entryList.html',
+          controller: 'CategoryFilter'
+        }).
+        when( '/category/:category/subcategory/:subcategory', {
           templateUrl: '/app/partials/entryList.html',
           controller: 'CategoryFilter'
         }).
@@ -28,33 +52,44 @@
           controller: 'CategoryFilter'
         }).
         otherwise({
-          redirectTo: '/category/:category'
+          redirectTo: '/'
         });
+      $locationProvider.html5Mode( true );
+  }]);
+  
+  fieldGuide.controller( 'StartCtrl', [ '$scope', '$timeout', function( $scope, $timeout ){
+    $scope.types = types;
+    $scope.habitats = habitats;
+    $scope.navItems = _.map( $scope.types, function( type ){
+      return type.name;
+    });
+    $scope.navItems = $scope.navItems.concat( habitats );
+    $timeout( function(){
+       $( document ).foundation();
+    });
+  }]);
+  
+  fieldGuide.controller( 'NavCtrl', [ '$scope', function( $scope ){
+    $scope.types = types;
+    $scope.habitats = habitats;
+    $scope.navItems = _.map( $scope.types, function( type ){
+      return type.name;
+    });
+    $scope.navItems = $scope.navItems.concat( habitats );
   }]);
 	
-  category.controller('CategoryFilter', ['$scope', '$http', 'FilterOptions', '$routeParams', function( $scope, $http, FilterOptions, $routeParams ){
-    $scope.options = options;
-    $scope.type = $( "#type" ).text();
-    $scope.subtype = "";
+  fieldGuide.controller('CategoryFilter', ['$scope', '$http', '$routeParams', function( $scope, $http, $routeParams ){
+    $scope.options = _.find( types, function( type ){
+     return type.name === $routeParams.category;
+    }).subtypes;
+    $scope.category = $routeParams.category;
+    $scope.subcategory = $routeParams.subcategory ? $routeParams.subcategory : 'All' ;
     $scope.url_title = $routeParams.url_title;
-
-   /* FilterOptions.getOptions().then( function( data ){
-      $.each( data, function( key, value ){
-        if( value.parentType == $scope.type ){
-          value.title = _.unescape( value.title );
-          value.parentType = _.unescape( value.parentType );
-          $scope.options.push( { "title" : value.title, "parentType" : value.parentType } )
-        }
-      });
-    }); */
-    
     $http.jsonp('http://www.chesapeakebay.net/site/API_test?callback=JSON_CALLBACK').success( function( data ){
-      console.log( data );
       $scope.entries = data;
-    });    
+    });
+    $scope.$watch( $routeParams.category );
 	}]);
 
-  category.run(['$route', function( $route ){
-    $route.reload();
-  }]);
+
 })();
