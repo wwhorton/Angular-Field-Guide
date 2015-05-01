ba81018daaac0c6abbcc572bb53ba81d9092ee92
Bill Whorton <wwhorton@chesapeakebay.net>
2015-05-01 13:16:28 -0400

    Adds logo image

---
+++ dist/js/services.js
@@ -0,0 +1,151 @@
+'use strict';
+( function(){
+  
+  var fieldGuideServices = angular.module( 'fieldGuideServices', [ 'angular-md5' ] );
+
+  fieldGuideServices.factory( 'getEntries', [ '$http', function( $http ){
+    var promise;
+    var getEntries = function(){
+      if( !promise ){
+        promise = $http.jsonp('http://www.chesapeakebay.net/site/API_test?callback=JSON_CALLBACK');
+      }
+      return promise;
+    };
+    return getEntries;
+  }]);
+
+  fieldGuideServices.factory( 'renderHtml', [ '$sce', function( $sce ){
+    var renderHtml = function( raw_html ){
+      return $sce.trustAsHtml( raw_html );
+    };
+    return renderHtml;
+  }]);
+
+  fieldGuideServices.factory( 'renderSrc', [ '$sce', function( $sce ){
+    var renderSrc = function( raw_html ){
+      return $sce.trustAsResourceUrl( raw_html );
+    };
+    return renderSrc;
+  }]);
+
+  fieldGuideServices.factory( 'critterOfTheMonth', function(){
+    var getCritter = function( critters ) {
+      var monthlyCritter = _.filter( critters, function( critter ){
+        return critter.status === 'critterofthemonth';
+      });
+      monthlyCritter = _.sortBy( monthlyCritter, 'edit_date' );
+      return monthlyCritter[monthlyCritter.length - 1];
+    };
+    return getCritter;
+  });
+
+    fieldGuideServices.factory( 'relatedCritters', [ '$rootScope', 'getEntries', 'matchCategory', function( $rootScope, getEntries, matchCategory ){
+    return function( entries, critter ){
+      var filtered = [];
+      var categoryToMatch = _.find( critter.categories, function( category ){
+        var result;
+        function matchSubtypes( subtypeArray ){
+          for( var i = 0; i < subtypeArray.length; i++){
+            if( matchCategory( subtypeArray[i].name, category.category_name ) ){
+              result = true;
+            }
+          }
+        }
+        for( var i = 0; i < $rootScope.types.length; i++ ){
+          if( $rootScope.types[i].subtypes ){
+            if ( matchCategory( $rootScope.types[i].name, category.category_name ) ) { result = true; }
+            matchSubtypes( $rootScope.types[i].subtypes );
+          } else { 
+            if ( matchCategory( $rootScope.types[i].name, category.category_name ) ) { result = true; }
+          }
+        }
+        return result;
+      }).category_name;
+      
+      entries.forEach( function( entry ){
+        entry.categories.forEach( function( category ){
+          if( category.category_name.toUpperCase() === categoryToMatch.toUpperCase() ){
+            filtered.push( entry );
+          }
+        });
+      });
+      return filtered;
+    };  
+  }]);
+
+  fieldGuideServices.factory( 'matchCategory', function(){
+    return function( cat, category ){
+      if( cat === category ){
+        return true;
+      }
+    };
+  });  
+  fieldGuideServices.factory( 'makeButtons', [ '$rootScope', function( $rootScope ){
+    var makeButtons = function( critter ){
+      critter.buttons = {};
+      critter.buttons.type = _.find( critter.categories, function( category ){
+          for( var i = 0; i < $rootScope.types.length; i++ ){
+           if( $rootScope.types[i].name === category.category_name ){
+             return true;
+           }
+         }
+      }).category_name;
+      critter.buttons.habitats = _.filter( critter.categories, function( category ){
+          for( var i = 0; i < $rootScope.habitats.length; i++ ){
+           if( $rootScope.habitats[i].name === category.category_name ){
+             return true;
+           }
+         }
+      });    
+      critter.buttons.subtype = _.find( critter.categories, function( category ){
+        var results;
+        function matchCategory( subtype ){
+          if( subtype.name === category.category_name ){
+            results = true;
+          }
+        }
+        
+        for( var i = 0; i < $rootScope.types.length; i++ ){
+            _.compact( $rootScope.types[i].subtypes ).forEach( matchCategory );
+        }
+        return results;
+      });
+      critter.buttons.subtype = ( critter.buttons.subtype ) ? critter.buttons.subtype.category_name : undefined;
+    };
+    return makeButtons;
+  }]);
+
+  fieldGuideServices.factory( 'getFlickr', [ '$http', 'md5', function( $http, md5 ){
+    return function( tags ){
+      var apiSig = '589af94b6012d347api_keyc03d2c380487bc102a9213f8487b891dauth_token72157627022294584-5cb5ec33745f610cextrasdescriptionformatrestmethodflickr.photos.searchtags' + tags + 'user_id29388462@N06';
+      apiSig = md5.createHash( apiSig );     
+      return $http( {
+        url:  'https://api.flickr.com/services/rest/',
+        params: {
+          'method' : 'flickr.photos.search',
+          'api_key' : 'c03d2c380487bc102a9213f8487b891d',
+          'user_id' : '29388462@N06',
+          'tags' : tags,
+          'extras' : 'description',
+          'format' : 'rest',
+          'auth_token' : '72157627022294584-5cb5ec33745f610c',
+          'api_sig' : apiSig
+        }
+      });
+    };
+
+     
+  }]);
+
+  fieldGuideServices.factory( 'equalize', ['$timeout', function( $timeout ){
+    return function(){
+      $timeout( function(){
+        $(document).foundation( {
+          equalizer: {
+            equalize_on_stack: true
+          }
+        }, 'equalizer', 'reflow' );
+      }, 100 );
+    };
+  }]);
+})();
---
+++ test/unit/filter-spec.js
@@ -0,0 +1,96 @@
+
+describe( 'Filter testing', function(){
+  var $filter, filter;
+  var testArray = [
+                        { title: 'Farms',
+                          category_name: 'Solvents',
+                          fieldguide_appearance: 'A lot of stuff and things...',
+                          fieldguide_scientific_name: 'science 1',
+                          fieldguide_other_facts: 'facts facts facts'
+                        },
+                        { title: 'Penguins',
+                          category_name: 'Penguins',
+                          fieldguide_appearance: 'Lorem ipsum is so 90s.',
+                          fieldguide_scientific_name: 'science 2',
+                          fieldguide_other_facts: 'Stuff to know'
+                        },
+                        { title: '3r8789',
+                          fieldguide_appearance: 'baldkhjlkhjrthr',
+                          fieldguide_scientific_name: 'science 3',
+                          fieldguide_other_facts: 'No facts are fun.'
+                        },
+                        { title: 'Dogs',
+                          fieldguide_appearance: 'Blah blah blah!',
+                          fieldguide_scientific_name: 'Blahicus blahface',
+                          fieldguide_other_facts: 'Things you didn\'t know before but do now...'
+                        }
+                   ];
+  
+  beforeEach( function(){
+    module( 'fieldGuideFilters' );
+    inject( function( _$filter_ ){
+      $filter = _$filter_;
+      
+    });
+  });
+
+  describe( 'entriesByKeyword filter', function(){
+
+    var queryTitle = 'Farms', queryAppearance = 'bald', queryScience = 'Blahicus', queryFacts = 'fun', queryMultiple = 'science';
+    
+    beforeEach( function(){
+      filter = $filter( 'entriesByKeyword' );
+    });
+    
+    it( 'should exist as a filter', function(){
+      expect( filter ).toBeDefined();
+    });
+    
+    it( 'should return an array of any objects that have a title containing the queried text', function(){
+      var filteredArray = filter( testArray, queryTitle );
+      expect( filteredArray.length ).toEqual( 1 );
+      expect( filteredArray[0].title ).toEqual( queryTitle );
+    });
+    
+    it( 'should return an array of any objects that have an appearance containing the queried text', function(){
+      var filteredArray = filter( testArray, queryAppearance );
+      expect( filteredArray.length ).toEqual( 1 );
+      expect( filteredArray[0].fieldguide_appearance ).toContain( queryAppearance );
+    });
+    
+    it( 'should return an array of any objects that have a scientific name containing the queried text', function(){
+      var filteredArray = filter( testArray, queryScience );
+      expect( filteredArray.length ).toEqual( 1 );
+      expect( filteredArray[0].fieldguide_scientific_name ).toContain( queryScience );
+    });
+    
+    it( 'should return an array of any objects that have an "other facts" field containing the queried text', function(){
+      var filteredArray = filter( testArray, queryFacts );
+      expect( filteredArray.length ).toEqual( 1 );
+      expect( filteredArray[0].fieldguide_other_facts ).toContain( queryFacts );
+    });
+    
+    it( 'should return an array of multiple objects if more than one has a field containing the queried text', function(){
+      var filteredArray = filter( testArray, queryMultiple );
+      expect( filteredArray.length ).toBeGreaterThan( 1 );
+    });   
+  });
+
+  describe( 'entryByTitle filter', function(){
+    
+    beforeEach( function(){
+      filter = $filter( 'entryByTitle' );
+    });
+    
+    it( 'should have an "entryByTitle" filter', function(){
+      expect( $filter( 'entryByTitle' ) ).toBeDefined();
+    });
+    
+    it( 'should return single object from array whose title matches the query', function(){
+      var testTitle = 'Farms';
+      var filteredData = filter( testArray, testTitle );
+      expect( filteredData.title ).toEqual( testTitle );
+    });
+  });
+  
+});
